31dbe3ad1efcd36fcee5d831e3bc1c031927188e
nalinsaxena22 <nalin.saxena0@gmail.com>
2026-01-18 15:51:42 +0530

    Update init.database.sql

--- scripts/init.database.sql
+++ scripts/init.database.sql
@@ -13,3 +13,29 @@ WARNING:
     and ensure you have proper backups before running this script.
 */
 
+USE master;
+GO
+
+-- Drop and recreate the 'EmAt' database.
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'EmAt')
+BEGIN
+	ALTER DATABASE EmAt SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE EmAt;
+END;
+GO
+
+CREATE DATABASE EmAt;
+GO
+
+USE EmAt;
+GO
+
+-- Create schemas.
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
