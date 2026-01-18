bdd3cb6f51a341814d85851b59a67be5b4fe2b89
nalinsaxena22 <nalin.saxena0@gmail.com>
2026-01-18 16:23:08 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -9,3 +9,46 @@ Script Purpose:
 ===============================================================================
 */
 
+IF OBJECT_ID ('bronze.emp_att_raw', 'U') IS NOT NULL
+	DROP TABLE bronze.emp_att_raw;
+GO
+
+
+CREATE TABLE bronze.emp_att_raw (
+age										INT,
+attrition								NVARCHAR(50),
+business_travel							NVARCHAR(50),
+daily_rate								INT,
+department								NVARCHAR(50),
+distance_from_home						INT,
+education								INT,
+education_field							NVARCHAR(50),
+employee_count							INT,
+employee_number							INT,
+environment_satisfaction				INT,
+gender									NVARCHAR(50),
+hourly_rate								INT,
+job_involvement							INT,
+job_level								INT,
+job_role								NVARCHAR(50),
+job_satisfaction						INT,
+marital_status							NVARCHAR(50),
+monthly_income							INT,
+monthly_rate							INT,
+num_companies_worked					INT,
+over_18									NVARCHAR(50),
+overtime								NVARCHAR(50),
+percent_salary_hike						INT,
+performance_rating						INT,
+relationship_satisfaction				INT,
+standard_hours							INT,
+stock_option_level						INT,
+total_working_years						INT,
+training_times_last_year				INT,
+work_life_balance						INT,
+years_at_company						INT,
+years_in_current_role					INT,
+years_since_last_promotion				INT,
+years_with_curr_manager					INT
+);
+GO
