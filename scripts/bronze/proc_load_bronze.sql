aa7e32ec5ccba0c9b638e5b805b84191b3c93f92
nalinsaxena22 <nalin.saxena0@gmail.com>
2026-01-19 14:04:45 +0530

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -17,4 +17,38 @@ Usage Example:
 ===============================================================================
 */
 
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+	DECLARE @start_time DATETIME, @end_time DATETIME;
+	BEGIN TRY
+		PRINT '==================================';
+		PRINT 'Loading Bronze Layer';
+		PRINT '==================================';
 
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.emp_att_raw';
+		TRUNCATE TABLE bronze.emp_att_raw;
+		PRINT '>> Inserting Data Into: bronze.emp_att_raw';
+		BULK INSERT bronze.emp_att_raw
+				FROM 'D:\SQL files\HR-Employee-Attrition.csv'
+				WITH (
+				FIRSTROW = 2,
+				FIELDTERMINATOR = ',',
+				TABLOCK
+				);
+		SET @end_time = GETDATE();
+
+		PRINT '==================================';
+		PRINT 'Loading Bronze Layer is completed';
+		PRINT '		- Total Load Douration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '==================================';
+	END TRY
+	BEGIN CATCH
+	PRINT '==================================';
+	PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+	PRINT 'Error Message' + ERROR_MESSAGE();
+	PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+	PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+	PRINT '==================================';
+	END CATCH
+END
